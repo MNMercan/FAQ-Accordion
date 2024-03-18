0503ebd365d583a0620d3771506f98389679c211
Numan <numan.mercan24@gmail.com>
2024-03-18 07:19:43 +0300

    feat(refactor): edited with grid structure, now it is more smooth

---
+++ old-script.js
@@ -0,0 +1,23 @@
+const items = document.querySelectorAll('.list-item');
+const toggleButtons = document.querySelectorAll('.icon-toggle > img')
+const plus = "http://127.0.0.1:5500/assets/images/icon-plus.svg"
+const minus = "http://127.0.0.1:5500/assets/images/icon-minus.svg"
+console.log(toggleButtons);
+
+items.forEach((item) => {
+    item.addEventListener('click', () => {
+        console.log(item.classList)
+        item.classList.toggle('active');
+    })
+})
+toggleButtons.forEach((item) => {
+    let toggle = false;
+    item.addEventListener('click', () => {
+        if (toggle) {
+            item.src = plus;
+        } else {
+            item.src = minus;
+        }
+        toggle = !toggle;
+    })
+})
--- script.js
+++ script.js
@@ -1,23 +1,23 @@
-const items = document.querySelectorAll('.list-item');
-const toggleButtons = document.querySelectorAll('.icon-toggle > img')
-const plus = "http://127.0.0.1:5500/assets/images/icon-plus.svg"
-const minus = "http://127.0.0.1:5500/assets/images/icon-minus.svg"
-console.log(toggleButtons);
+const items = document.querySelectorAll(".item-header");
+const icons = document.querySelectorAll(".icon-toggle > img");
+const iconPlus = "http://127.0.0.1:5500/assets/images/icon-plus.svg";
+const iconMinus = "http://127.0.0.1:5500//assets/images/icon-minus.svg";
+const toggleOpen = (event) => {
+  const { currentTarget } = event;
+  console.log(currentTarget.children);
+  currentTarget.parentElement.classList.toggle("open");
+  setTimeout(() => {
+    currentTarget.children[1].src =
+      currentTarget.children[1].src === iconPlus ? iconMinus : iconPlus;
+    currentTarget.children[1].style.rotate = "90deg";
 
-items.forEach((item) => {
-   item.addEventListener('click', () => {
-    console.log(item.classList)
-    item.classList.toggle('active');
-   })
-})
-toggleButtons.forEach((item) => {
-    let toggle = false;
-    item.addEventListener('click', () => {
-        if(toggle){
-            item.src = plus;
-        }else{
-            item.src = minus;
-        }
-        toggle = !toggle;
-    })
-})
+    console.log(currentTarget.children[1].style);
+  }, 150);
+};
+
+const bindClickEvent = (target) => {
+  console.log(target);
+  target.addEventListener("click", toggleOpen);
+};
+
+items.forEach(bindClickEvent);
